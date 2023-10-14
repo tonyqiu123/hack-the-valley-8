b6cb33e5b0235f1d6d2fd762319a4056bed9c7d4
eysix <andrewau121@gmail.com>
2023-10-14 13:51:44 -0400

    Add homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,30 @@
-
+import '../css/Home.css'
+import { useState, useEffect } from 'react'
 
 const Home = () => {
     return (
-        <h1>hi</h1>
+        <div className="home-container">
+            <div className="homeLeftColumn">
+                <div className="text-container">
+                    <h1>helloooooo</h1>
+                </div>
+            </div>
+            <div className="homeRightColumn">
+                <div className="right-container">
+                    <div className="right-header">
+                        <h1>Get started</h1>
+                    </div>
+                    <div className="right-bottom-header">
+                        <a href="/login">
+                            <button className="login-button">Login</button>
+                        </a>
+                        <a href="/signup">
+                            <button className="login-button">Sign Up</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
