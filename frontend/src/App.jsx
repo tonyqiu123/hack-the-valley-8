c9b90e897438e4e4d7dbc2407c21411e3bb35bc0
Tony Qiu <tonyqiu12345@gmail.com>
2023-10-13 22:15:22 -0400

    pushing browser router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home'
 import Chat from './pages/Chat'
+import NotFound from './pages/NotFound'
 
 function App() {
 
--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom"
 
 
 const NotFound = () => {
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', gap: '8px', alignItems: 'center', justifyContent: 'center', height: '100dvh', width: '100dvw' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '12px', alignItems: 'center', justifyContent: 'center', height: '100dvh', width: '100dvw' }}>
             <h1>404</h1>
             <p>Page not found.</p>
+            <Link style={{ marginTop:'16px' }} to='/'><p>Take me home</p></Link>
         </div>
     )
 }
